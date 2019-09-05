c7659352112c631e1b6f661d7093910f2524ffb6
P147970 <P147970@SKTP147970PN001.SKT.AD>
2019-09-05 10:03:17 +0900

    ..

---
+++ src/com/sccomz/comm/App.scala
@@ -0,0 +1,25 @@
+package com.nexweb.xtractor.dw.stat.spark.common
+
+import java.util.Calendar
+import java.text.SimpleDateFormat
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.types.StringType
+import org.apache.spark.sql.types.StructField
+
+object App {
+
+  /**********************************************************
+   * 전역 변수 세팅
+   **********************************************************/
+  //val localBasicPath       = "file:////data/xtractor/entity"
+  val localBasicPath       = "file:////home/xtractor/entity"
+
+  val hdfsLogBasicPath     = "/user/xtractor/data/entity"
+  val hdfsParquetBasicPath = "/user/xtractor/parquet/entity"
+
+  val localPcBasicPath     = "file:////c:/Users/P136391/Documents/data"
+
+  val spark = SparkSession.builder().appName("Batch").getOrCreate() //.config("spark.master","local")
+
+}
---
+++ src/com/sccomz/schedule/control/RealJob.scala
@@ -0,0 +1,12 @@
+package com.sccomz.schedule.control
+
+object RealJob {
+
+  def main(args: Array[String]): Unit = {
+
+    //--------------------------------------
+    println("RealJob 시작");
+    //--------------------------------------
+  }
+
+}
