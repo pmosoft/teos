8d84df72d28699b43c1857c2ae1b9b40b2e4f759
P147970 <P147970@SKTP147970PN001.SKT.AD>
2019-10-16 12:30:29 +0900

    ..

--- src/com/sccomz/scala/comm/App.scala
+++ src/com/sccomz/scala/comm/App.scala
@@ -51,17 +51,21 @@ object App {
    **********************************************************/
   val dbDriverOra      = "oracle.jdbc.driver.OracleDriver";
   val dbUrlOra         = "jdbc:oracle:thin:@192.168.0.6:1521/ORCL";
+           if(ip=="192.168.0.6")   "jdbc:oracle:thin:@192.168.0.6:1521/ORCL"
+      else if(ip=="192.168.73.71") "jdbc:oracle:thin:@192.168.0.6:1521/ORCL"
+      else if(ip=="150.23.21.44")  "jdbc:oracle:thin:@localhost:9951/IAMLTE"
+      else                         "jdbc:oracle:thin:@192.168.0.6:1521/ORCL"
   val dbUserOra        = "cellplan";
   val dbPwOra          = "cell_2012";
 
   val dbDriverPost     = "org.postgresql.Driver";
   val dbUrlPost        =
-           if(ip=="192.168.0.6")   "jdbc:postgresql://185.15.16.156:5432/postgres"
-      else if(ip=="150.23.21.44")  "jdbc:postgresql://localhost:55432/postgres"
+           if(ip=="192.168.0.6")   "jdbc:postgresql://localhost:5432/postgres"
       else if(ip=="192.168.73.71") "jdbc:postgresql://185.15.16.156:5432/postgres"
+      else if(ip=="150.23.21.44")  "jdbc:postgresql://localhost:55432/postgres"
       else                         "jdbc:postgresql://185.15.16.156:5432/postgres";
     ;
   val dbUserPost       = "postgres";
   val dbPwPost         = "postgres";
-
+  
 }
