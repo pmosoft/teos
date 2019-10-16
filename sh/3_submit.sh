3cf9b83a5a368d646fa22b3ad51e7cd69460504a
Administrator <Administrator@DESKTOP-6V7L04O>
2019-10-16 11:31:43 +0900

    ..

--- sh/3_submit.sh
+++ sh/3_submit.sh
@@ -1,3 +1,5 @@
+
+
 spark2-shell --deploy-mode client --driver-memory 2g --executor-memory 10g --jars /home/teos/lib/ojdbc7-12.1.0.2.jar,/home/teos/bin/teos.jar
 
 spark2-shell --deploy-mode client --master local[*] --driver-memory 2g --executor-memory 10g --jars /home/teos/lib/ojdbc7-12.1.0.2.jar,/home/teos/bin/teos.jar
@@ -12,4 +14,5 @@ spark2-shell --jars /home/str/app/bin/str.jar
 
 #spark2-shell --deploy-mode client --driver-memory 2g --executor-memory 10g --jars /home/xtractor/commoc/ojdbc6.jar, /home…./xtractorSpart.jar
 
+spark2-submit --master yarn --deploy-mode cluster --queue xtractor --executor-cores 4 --jars /home/xtractor/commoc/ojdbc6.jar --class com��.PvStat /home��./xtractorSpart.jar 20181028
 
