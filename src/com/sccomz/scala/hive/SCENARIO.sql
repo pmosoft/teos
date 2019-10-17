62dd031480e02b4c77c77d1d93d6e52fb125b480
P148312 <P148312@SKTP148312PN002.SKT.AD>
2019-10-17 09:56:44 +0900

    ..

--- src/com/sccomz/scala/hive/SCENARIO.sql
+++ src/com/sccomz/scala/hive/SCENARIO.sql
@@ -1,33 +1,66 @@
-DROP TABLE SCHEDULE;
+DROP TABLE SCENARIO;
 
-CREATE EXTERNAL TABLE SCHEDULE (
-  TYPE_CD                   STRING
-, SCENARIO_ID               INT
-, USER_ID                   STRING
-, PRIORITIZE                STRING
-, PROCESS_CD                STRING
-, PROCESS_MSG               STRING
-, SCENARIO_PATH             STRING
-, REG_DT                    STRING
-, MODIFY_DT                 STRING
-, RETRY_CNT                 INT
-, SERVER_ID                 STRING
-, BIN_X_CNT                 INT
-, BIN_Y_CNT                 INT
-, RU_CNT                    INT
-, ANALYSIS_WEIGHT           INT
-, PHONE_NO                  STRING
-, RESULT_TIME               INT
-, TILT_PROCESS_TYPE         INT
-, GEOMETRYQUERY_SCHEDULE_ID INT
-, RESULT_BIT                STRING
-, INTERWORKING_INFO         STRING
+CREATE EXTERNAL TABLE SCENARIO (
+  SCENARIO_ID                      INT
+, SCENARIO_NM                      INT
+, USER_ID                          STRING
+, SYSTEM_ID                        INT
+, NETWORK_TYPE                     INT
+, SIDO_CD                          STRING
+, SIGUGUN_CD                       STRING
+, DONG_CD                          STRING
+, SIDO                             STRING
+, SIGUGUN                          STRING
+, DONG                             STRING
+, STARTX                           INT
+, STARTY                           INT
+, ENDX                             INT
+, ENDY                             INT
+, FA_MODEL_ID                      INT
+, FA_SEQ                           INT
+, SCENARIO_DESC                    STRING
+, USE_BUILDING                     INT
+, USE_MULTIFA                      INT
+, PRECISION                        INT
+, PWRCTRLCHECKPOINT                INT
+, MAXITERATIONPWRCTRL              INT
+, RESOLUTION                       INT
+, MODEL_RADIUS                     INT
+, REG_DT                           STRING
+, MODIFY_DT                        STRING
+, UPPER_SCENARIO_ID                INT
+, FLOORBUILDING                    INT
+, FLOOR                            INT
+, FLOORLOSS                        INT
+, SCENARIO_SUB_ID                  INT
+, SCENARIO_SOLUTION_NUM            INT
+, LOSS_TYPE                        INT
+, RU_CNT                           INT
+, MODIFY_YN                        STRING
+, BATCH_YN                         STRING
+, TM_STARTX                        INT
+, TM_STARTY                        INT
+, TM_ENDX                          INT
+, TM_ENDY                          INT
+, REAL_DATE                        STRING
+, REAL_DRM_FILE                    STRING
+, REAL_COMP                        STRING
+, REAL_CATT                        STRING
+, REAL_CATY                        STRING
+, BLD_TYPE                         STRING
+, RET_PERIOD                       INT
+, RET_END_DATETIME                 STRING
+, BUILDINGANALYSIS3D_YN            STRING
+, BUILDINGANALYSIS3D_RESOLUTION    INT
+, AREA_ID                          INT
+, BUILDINGANALYSIS3D_RELATED_YN    STRING
+, RELATED_ANALYSIS_COVLIMITRSRP    INT
 )
-PARTITIONED BY (SCHEDULE_ID INT)
-COMMENT 'SCHEDULE'
+PARTITIONED BY (SCENARIO_ID INT)
+COMMENT 'SCENARIO'
 STORED AS PARQUET
-LOCATION '/user/teos/parquet/entity/SCHEDULE';
+LOCATION '/user/teos/parquet/entity/SCENARIO';
 
 --ALTER TABLE SCHEDULE ADD PARTITION (SCHEDULE_ID=8459967);
-ALTER TABLE SCHEDULE ADD PARTITION (SCHEDULE_ID=8459967) LOCATION '/user/teos/parquet/entity/SCHEDULE/SCHEDULE_ID=8459967';
-ALTER TABLE SCHEDULE DROP PARTITION (SCHEDULE_ID=8459967);
+ALTER TABLE SCENARIO ADD PARTITION (SCENARIO_ID=8459967) LOCATION '/user/teos/parquet/entity/SCENARIO/SCENARIO_ID=8459967';
+ALTER TABLE SCENARIO DROP PARTITION (SCENARIO_ID=8459967);
