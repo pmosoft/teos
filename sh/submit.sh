770988572891172a203ab73f014d7c4e2825db13
P147970 <P147970@SKTP147970PN001>
2019-10-28 14:52:38 +0900

    ..

--- sh/submit.sh
+++ sh/submit.sh
@@ -1,6 +1,10 @@
 spark2-shell --master local[*] --driver-memory 2g --executor-memory 10g --jars /home/icpap/bin/teos.jar,/home/icpap/lib/ojdbc7.jar,/home/icpap/lib/postgresql4.jar,/home/icpap/lib/hiveJdbc11.jar,/home/icpap/lib/ImpalaJDBC41.jar
 spark2-shell --deploy-mode client --driver-memory 2g --executor-memory 10g --jars /home/icpap/bin/teos.jar,/home/icpap/lib/ojdbc7.jar,/home/icpap/lib/postgresql4.jar,/home/icpap/lib/hiveJdbc11.jar,/home/icpap/lib/ImpalaJDBC41.jar
 
+spark2-submit --master local[*] --driver-memory 2g --executor-memory 10g --jars /home/icpap/lib/ojdbc7.jar,/home/icpap/lib/postgresql4.jar,/home/icpap/lib/hiveJdbc11.jar --class com.sccomz.scala.etl.extract.oracle.ExtractOraManager /home/icpap/bin/teos.jar 
+
+spark2-submit --master local[*] --driver-memory 2g --executor-memory 10g --jars /home/icpap/lib/ojdbc7.jar,/home/icpap/lib/postgresql4.jar,/home/icpap/lib/hiveJdbc11.jar --class com.sccomz.scala.etl.extract.oracle.MakeBinFile1 /home/icpap/bin/teos.jar 
+
 spark2-shell --master local[*] --jars /home/icpap/bin/teos.jar,/home/icpap/lib/ojdbc7.jar,/home/icpap/lib/postgresql4.jar,/home/icpap/lib/hiveJdbc11.jar,/home/icpap/lib/ImpalaJDBC41.jar
 
 spark2-shell --deploy-mode client --master local[*] --driver-memory 2g --executor-memory 10g --jars /home/icpap/lib/ojdbc7.jar,/home/icpap/lib/hiveJdbc11.jar, /home/icpap/bin/teos.jar
@@ -12,3 +16,7 @@ spark2-shell --jars /home/str/app/bin/str.jar
 
 #spark2-shell --deploy-mode client --driver-memory 2g --executor-memory 10g --jars /home/xtractor/commoc/ojdbc6.jar, /home…./xtractorSpart.jar
 spark2-submit --master yarn --deploy-mode cluster --queue xtractor --executor-cores 4 --jars /home/xtractor/commoc/ojdbc6.jar --class com��.PvStat /home��./xtractorSpart.jar 20181028
+
+
+
+--master yarn --deploy-mode cluster --queue xtractor --executor-cores 4 --jars /home/xtractor/commoc/ojdbc6.jar 
