cb12aca6b9d031960088abdb74cf9b523fc5e7ec
P147970 <P147970@SKTP147970PN001>
2019-10-31 13:43:37 +0900

    ..

--- sql/pq03.sql
+++ sql/pq03.sql
@@ -1,7 +1,14 @@
 INSERT INTO I_MOBILE_PARAMETER VALUES ( 5103982,9,14,'NR 3.5G MOBILE','unknown',-40,23,0,-113.4,1.5,0,0,0,0,5,5,0,4);
+commit;
+select * from I_MOBILE_PARAMETER;
+
+drop table I_du;
+
+SELECT * FROM public.i_scenario;
+
+delete from public.i_scenario;
 
 commit;
 
-select * from I_MOBILE_PARAMETER;
 
-
+SELECT * FROM public.i_scenario;
