7297082804332533d06e1a51a2e1d2bad87a2fc1
P148312 <P148312@SKTP148312PN002>
2019-11-19 17:52:15 +0900

    ..

--- src/com/sccomz/scala/job/spark/eng/RsrpPilot2.scala
+++ src/com/sccomz/scala/job/spark/eng/RsrpPilot2.scala
@@ -4,10 +4,6 @@ import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.FileSystem
 import org.apache.hadoop.fs.Path
 import org.apache.spark.sql.SparkSession
-import com.sccomz.scala.comm.App
-import java.sql.DriverManager
-import java.sql.Statement
-
 
 /*
  * 설    명 :
@@ -27,42 +23,42 @@ RsrpPilot.execute("8463189");
 
  */
 
-object RsrpPilot2 {
+object RsrpPilot {
 
-  def main(args: Array[String]): Unit = {
-    var scheduleId = if (args.length < 1) "" else args(0);
-    execute(scheduleId);
-  }
+def main(args: Array[String]): Unit = {  
+  var scheduleId = if (args.length < 1) "" else args(0);
+  execute(scheduleId);
+}     
 
-  def execute(scheduleId: String) = {
-    executeSql(scheduleId);
-  }
+def execute(scheduleId:String) = {
+  val spark: SparkSession = SparkSession.builder().master("yarn").appName(this.getClass.getName).config("spark.sql.warehouse.dir","/TEOS/warehouse").enableHiveSupport().getOrCreate();
+  executeSql(spark, scheduleId);
+  spark.close();
+}
 
-  //  * * * Hive Query * * *
-  def executeSql(scheduleId: String) = {
-    Class.forName(App.dbDriverHive);
-    var con = DriverManager.getConnection(App.dbUrlHive,App.dbUserHive,App.dbPwHive);
-    var stat:Statement=con.createStatement();
-    //var scheduleId = "8463189";
-
-    var objNm = "RESULT_NR_2D_RSRPPILOT_RU"
-    //------------------------------------------------------
+def executeSql(spark: SparkSession, scheduleId:String) = {
+//var scheduleId = "8463189"; 
+  
+var objNm = "RESULT_NR_2D_RSRPPILOT_RU"
+//------------------------------------------------------
     println(objNm + " 시작");
-    //------------------------------------------------------
-    var qry = s"""ALTER TABLE ${objNm} DROP IF EXISTS PARTITION (schedule_id=${scheduleId})"""; println(qry); stat.execute(qry);
-
-    //---------------------------------------------------
+//------------------------------------------------------
+var qry = "";
+  
+//---------------------------------------------------
     println("partiton 파일 삭제 및 drop table partition");
-    //---------------------------------------------------
-    val conf = new Configuration()
-    val fs = FileSystem.get(conf)
-    fs.delete(new Path(s"""/TEOS/warehouse/${objNm}/schedule_id=${scheduleId}"""), true)
-    qry = s"""set hive.exec.dynamic.partition.mode=nontrict"""; println(qry); stat.execute(qry);
+//---------------------------------------------------
+val conf = new Configuration()
+val fs = FileSystem.get(conf)
+fs.delete(new Path(s"""/TEOS/warehouse/${objNm}/schedule_id=${scheduleId}"""),true)
+import spark.implicits._
+import spark.sql
+qry = s"""ALTER TABLE I_${objNm} DROP IF EXISTS PARTITION (schedule_id=${scheduleId})"""; sql(qry);
 
-    //---------------------------------------------------
+//---------------------------------------------------
     println("insert partition table");
-    //---------------------------------------------------
-    qry = s"""
+//---------------------------------------------------
+qry = s"""
 WITH PARAM AS
 (
 SELECT b.scenario_id, a.schedule_id, b.ru_id, b.enb_id, b.sector_ord as cell_id,
@@ -81,15 +77,30 @@ SELECT b.scenario_id, a.schedule_id, b.ru_id, b.enb_id, b.sector_ord as cell_id,
 ),
 ANTGAIN AS
 (
-select schedule_id, ru_id,
-       rx_tm_xpos, rx_tm_ypos,
-       0 as antgain,
-       null as los,
-       null as pathloss,
-       null as plprime,
-       null as rsrppilot
-  from RESULT_NR_2D_PATHLOSS_RU
- where schedule_id = ${scheduleId}
+select a.scenario_id, a.schedule_id, a.ru_id,
+       a.rx_tm_xpos div b.resolution * b.resolution as rx_tm_xpos,
+       a.rx_tm_ypos div b.resolution * b.resolution as rx_tm_ypos,
+       a.antgain,
+       a.los, a.pathloss, a.plprime, a.rsrppilot -- 비교 검증용
+  from
+	(
+	--@@@
+	select --5104573 as scenario_id, 8460062 as schedule_id,
+	       scenario_id, schedule_id,
+	       ru_id, rx_tm_xpos, rx_tm_ypos,
+	       antgain, los, pathloss, plprime, rsrppilot
+--	  from RESULT_NR_2D_PILOT_RU_temp_8460964 -- 1RU
+--	  from RESULT_NR_2D_PILOT_RU_temp_8460968 --152RU(Radial)
+--	  from RESULT_NR_2D_PILOT_RU_temp_8460853 --152RU(BINtoBIN)
+	  from RESULT_NR_2D_PILOT_RU_temp_8463189 --154RU(Radial TRAFFIC)
+	) a left outer join
+	(  
+	select a.scenario_id, b.schedule_id, a.resolution
+	  from SCENARIO a, SCHEDULE b
+	 where b.schedule_id = 8463189
+	   and a.scenario_id = b.scenario_id
+	 limit 1
+	) b 
 ),
 PLPRIME_temp AS
 (
@@ -113,20 +124,30 @@ SELECT PATHLOSS.scenario_id, PATHLOSS.ru_id, PARAM.enb_id, PARAM.cell_id,
    and PATHLOSS.schedule_id = PARAM.schedule_id
    AND PATHLOSS.ru_id = PARAM.ru_id
 )
-insert into ${objNm} partition (schedule_id)
-select PLPRIME_temp.scenario_id, PLPRIME_temp.ru_id, PLPRIME_temp.enb_id, PLPRIME_temp.cell_id,
-       PLPRIME_temp.rx_tm_xpos, PLPRIME_temp.rx_tm_ypos,
-       PLPRIME_temp.los, PLPRIME_temp.pathloss,
-       ANTGAIN.antgain,
-       PLPRIME_temp.pathloss - nvl(ANTGAIN.antgain,0) as pathlossprime,
-       PLPRIME_temp.EIRPPerRB - 10. * log10(PLPRIME_temp.number_of_sc_per_rb) - (PLPRIME_temp.pathloss - nvl(ANTGAIN.antgain,0)) as RSRPPilot,
-       ANTGAIN.antgain, ANTGAIN.los, ANTGAIN.pathloss, ANTGAIN.plprime, ANTGAIN.rsrppilot,
-       PLPRIME_temp.schedule_id
+insert into I_${objNm} partition (schedule_id=${scheduleId})
+select PLPRIME_temp.scenario_id
+     , PLPRIME_temp.ru_id
+     , PLPRIME_temp.enb_id
+     , PLPRIME_temp.cell_id
+     , PLPRIME_temp.rx_tm_xpos
+     , PLPRIME_temp.rx_tm_ypos
+     , PLPRIME_temp.los
+     , PLPRIME_temp.pathloss
+     , ANTGAIN.antgain
+     , PLPRIME_temp.pathloss - nvl(ANTGAIN.antgain,0) as pathlossprime
+     , PLPRIME_temp.EIRPPerRB - 10. * log10(PLPRIME_temp.number_of_sc_per_rb) - (PLPRIME_temp.pathloss - nvl(ANTGAIN.antgain,0)) as RSRPPilot
+     , ANTGAIN.antgain
+     , ANTGAIN.los
+     , ANTGAIN.pathloss
+     , ANTGAIN.plprime
+     , ANTGAIN.rsrppilot
   from PLPRIME_temp left outer join ANTGAIN
+--  from PLPRIME_temp join ANTGAIN
     on (PLPRIME_temp.rx_tm_xpos = ANTGAIN.rx_tm_xpos and PLPRIME_temp.rx_tm_ypos = ANTGAIN.rx_tm_ypos and PLPRIME_temp.ru_id = ANTGAIN.ru_id)
-"""
-    println(qry); stat.execute(qry);
 
-  }
+"""
+println(qry); spark.sql(qry).take(100).foreach(println);
+
+}
 
 }
