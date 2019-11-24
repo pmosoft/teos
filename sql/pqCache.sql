c33a13b28a3f03efe6297e30cd4a355cea8c5b46
Administrator <Administrator@DESKTOP-6V7L04O>
2019-11-25 01:10:01 +0900

    ..

--- sql/pqCache.sql
+++ sql/pqCache.sql
@@ -76,7 +76,9 @@ GRANT ALL ON TABLE PUBLIC.I_SCENARIO_NR_RU_CACHE TO POSTGRES;
 -- DROP TABLE PUBLIC.I_NR_RU_CACHE;
 
 CREATE TABLE PUBLIC.I_NR_RU_CACHE (
-  RU_ID                         VARCHAR(48) NOT NULL
+  SCHEDULE_ID                   NUMERIC     NOT NULL
+, SCENARIO_ID                   NUMERIC     NOT NULL
+, RU_ID                         VARCHAR(48) NOT NULL
 , XPOSITION                     VARCHAR(40) NOT NULL
 , YPOSITION                     VARCHAR(40) NOT NULL
 , HEIGHT                        NUMERIC     NOT NULL
@@ -93,33 +95,79 @@ GRANT ALL ON TABLE PUBLIC.I_NR_RU_CACHE TO POSTGRES;
 
 ;
 
-
-with temp01 as
+-----------------------------------
+-- INS SCENARIO_NR_RU_CACHE
+-----------------------------------
+WITH TEMP01 AS
 (
-select a.scenario_id, a.ru_id, a.xposition, a.yposition, a.height, a.resolution, b.mobile_height
-from   i_scenario_nr_ru a,
-      (select scenario_id, max(height) as mobile_height
-       from i_mobile_parameter
-       where scenario_id = 5105173
-       group by scenario_id
-       ) b
-where  a.scenario_id = b.scenario_id
-and    a.scenario_id = 5105173
-) , temp02 as (
-select *
-from   temp01 a,
-       I_NR_RU_CACHE b
-where  a.ru_id
-and, a.xposition
-and, a.yposition
-and, a.height
-and, a.resolution
-and               , b.mobile_height
-
-
-
+SELECT A.SCENARIO_ID, A.RU_ID, A.XPOSITION, A.YPOSITION, A.HEIGHT, A.RESOLUTION, B.MOBILE_HEIGHT
+FROM   I_SCENARIO_NR_RU A,
+      (SELECT SCENARIO_ID, MAX(HEIGHT) AS MOBILE_HEIGHT
+       FROM I_MOBILE_PARAMETER
+       WHERE SCENARIO_ID = 5105173
+       GROUP BY SCENARIO_ID
+       ) B
+WHERE  A.SCENARIO_ID = B.SCENARIO_ID
+AND    A.SCENARIO_ID = 5105173
+) , TEMP02 AS (
+SELECT A.*
+     , CASE WHEN B.RU_ID IS NULL THEN 'N' ELSE 'Y' END AS CACHE_YN
+FROM   TEMP01 A
+       LEFT JOIN
+       I_NR_RU_CACHE B
+       ON  A.RU_ID         = B.RU_ID
+       AND A.XPOSITION     = B.XPOSITION
+       AND A.YPOSITION     = B.YPOSITION
+       AND A.HEIGHT        = B.HEIGHT
+       AND A.RESOLUTION    = B.RESOLUTION
+       AND A.MOBILE_HEIGHT = B.MOBILE_HEIGHT
 )
-select * from temp01
+INSERT INTO I_SCENARIO_NR_RU_CACHE SELECT * FROM TEMP02
+;
+
+-----------------------------------
+-- EXT SCENARIO_NR_RU
+-----------------------------------
+SELECT *
+FROM   I_SCENARIO_NR_RU A
+     , I_SCENARIO_NR_RU_CACHE B
+WHERE  A.SCENARIO_ID = B.SCENARIO_ID
+AND    A.RU_ID = B.RU_ID
+AND    B.CACHE_YN = 'N'
+;
+
+-----------------------------------
+-- INS RU_CACHE
+-----------------------------------
+INSERT INTO I_NR_RU_CACHE
+SELECT '' AS SCHDULE_ID
+     , A.*
+FROM   I_SCENARIO_NR_RU_CACHE A
+WHERE  A.SCENARIO_ID = ''
+AND    A.CACHE_YN = 'N'
+;
+
+-----------------------------------
+-- COPY NR_RU
+-----------------------------------
+INSERT INTO RESULT_NR_2D_LOS_RU (SCHEDULE_ID=${scheduleId},RU_ID=${ruId})
+SELECT RX_TM_XPOS
+     , RX_TM_YPOS
+     , RZ
+     , VALUE
+     , THETA
+     , PHI
+     , IS_BLD
+FROM   I_RESULT_NR_2D_LOS_RU A
+     , I_SCENARIO_NR_RU_CACHE B
+WHERE  A.SCENARIO_ID = B.SCENARIO_ID
+AND    A.RU_ID = B.RU_ID
+AND    B.CACHE_YN = 'Y'
+;
+
+--------------------------------------------------------------------------------
+)
+SELECT * FROM TEMP01
 ;
 
 DROP TABLE PUBLIC.I_SCENARIO_NR_RU_CACHE;
@@ -190,113 +238,5 @@ from i_mobile_parameter
 group by scenario_id
 ;
 
-select scenario_id
-     , COUNT(*)
-from job_dis
-group by scenario_id
-;
-
-
-select *
-from job_dis
-;
-
-
-select scenario_id,sector_id
-     , count(*)
-FROM LOS_ENG_RESULT
-group by scenario_id,sector_id
-;
-
-
-select scenario_id,ru_id,stat,cluster_name
-from job_dis
-where scenario_id = '5104573'
-;
-
-select scenario_id,cluster_name,ru_id,stat
-from job_dis
-where scenario_id = '5104573'
-order by scenario_id,cluster_name,ru_id,stat
-;
-
-select * from I_SCHEDULE
-;
-
-select * from i_du
-;
-
-SELECT
-       COALESCE(BIN_ID,'')        ||'|'||
-       BIN_X                      ||'|'||
-       BIN_Y                      ||'|'||
-       BIN_Z                      ||'|'||
-       COALESCE(BLD_ID,0)         ||'|'||
-       LOS                        ||'|'||
-       IN_BLD                     ||'|'||
-       THETA_DEG                  ||'|'||
-       PHI_DEG                    ||'|'||
-       SECTOR_X                   ||'|'||
-       SECTOR_Y                   ||'|'||
-       SECTOR_Z                   ||'|'||
-       '8460062'            ||'|'
-       ,BIN_ID
-FROM   LOS_ENG_RESULT
---WHERE  SCHEDULE_ID = 8460062
-WHERE  SCENARIO_ID IN (SELECT SCENARIO_ID FROM I_SCHEDULE WHERE SCHEDULE_ID = 8460062)
-AND    SECTOR_ID   = '1012242284'
-;
-
-drop table PUBLIC.JOB_DIS_ETL;
-
-CREATE TABLE PUBLIC.JOB_DIS_ETL (
-	SCHEDULE_ID TEXT NULL,
-	RU_ID       TEXT NULL,
-	STAT        INT4 NULL,
-	UPDATE_TIME TIMESTAMPTZ NULL,
-	CREATED_AT  TIMESTAMPTZ NULL DEFAULT CURRENT_TIMESTAMP
-);
-
-
-SELECT COUNT(A.RU_ID)                              AS RU_CNT
-     , COUNT(CASE WHEN A.STAT=3 THEN 1 ELSE 0 END) AS EXT_CNT
-     , COUNT(CASE WHEN B.STAT=4 THEN 1 ELSE 0 END) AS EXT_ING_CNT
-     , COUNT(CASE WHEN B.STAT=5 THEN 1 ELSE 0 END) AS EXT_DONE_CNT
-FROM   JOB_DIS A
-     , JOB_DIS_ETL B
-WHERE  A.SCENARIO_ID IN (SELECT cast(SCENARIO_ID as text) FROM I_SCHEDULE WHERE SCHEDULE_ID = 8460062)
-AND    B.SCHEDULE_ID = '8460062'
-;
-
-
-SELECT COUNT(A.RU_ID)                              AS RU_CNT
-     , COUNT(CASE WHEN A.STAT=3 THEN 1 ELSE 0 END) AS EXT_CNT
-     , COUNT(CASE WHEN B.STAT=4 THEN 1 ELSE 0 END) AS EXT_ING_CNT
-     , COUNT(CASE WHEN B.STAT=5 THEN 1 ELSE 0 END) AS EXT_DONE_CNT
-FROM   JOB_DIS A
-     , JOB_DIS_ETL B
-WHERE  A.SCENARIO_ID IN (SELECT cast(SCENARIO_ID as text) FROM I_SCHEDULE WHERE SCHEDULE_ID = 8460062)
-AND    B.SCHEDULE_ID = '8460062'
-;
-
-
-select A.RU_CNT , A.EXT_CNT, B.EXT_ING_CNT, B.EXT_DONE_CNT
-from  (
-		SELECT COUNT(A.RU_ID)                              AS RU_CNT
-		     , COUNT(CASE WHEN A.STAT=3 THEN 1 ELSE 0 END) AS EXT_CNT
-		FROM   JOB_DIS A
-		WHERE  A.SCENARIO_ID IN (SELECT cast(SCENARIO_ID as text) FROM I_SCHEDULE WHERE SCHEDULE_ID = 8460062)
-      ) A,
-     (
-		SELECT COUNT(CASE WHEN B.STAT=4 THEN 1 ELSE 0 END) AS EXT_ING_CNT
-		     , COUNT(CASE WHEN B.STAT=5 THEN 1 ELSE 0 END) AS EXT_DONE_CNT
-		FROM   JOB_DIS_ETL B
-		WHERE  B.SCHEDULE_ID = '8460062'
-	 ) B
-;
-
-
-SELECT cast(SCENARIO_ID as text) FROM I_SCHEDULE WHERE SCHEDULE_ID = 8460062
-;
 
 
