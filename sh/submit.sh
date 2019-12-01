fdf0e36609037f240cd9d01e7470cdbe8a68c37c
Administrator <Administrator@DESKTOP-6V7L04O>
2019-12-02 03:31:08 +0900

    ..

--- sh/submit.sh
+++ sh/submit.sh
@@ -8,7 +8,12 @@ spark2-shell --master yarn-client --jars /home/icpap/bin/teos1.jar,/home/icpap/l
 #############################################
 # Schedule
 #############################################
+spark2-submit --master local[*] --driver-memory 8g --jars /home/icpap/bin/teos1.jar,/home/icpap/lib/ojdbc7.jar,/home/icpap/lib/postgresql4.jar,/home/icpap/lib/hiveJdbc11.jar --class com.sccomz.scala.schedule.real.ScheduleDaemon /home/icpap/bin/teos.jar
 
+#############################################
+# Schedule Job
+#############################################
+spark2-submit --master local[*] --driver-memory 8g --jars /home/icpap/bin/teos1.jar,/home/icpap/lib/ojdbc7.jar,/home/icpap/lib/postgresql4.jar,/home/icpap/lib/hiveJdbc11.jar --class com.sccomz.scala.schedule.real.ExecuteJob /home/icpap/bin/teos.jar 스케줄ID 시나리오ID
 
 #############################################
 # Eng
@@ -24,7 +29,6 @@ spark2-submit --master yarn --class com.sccomz.scala.job.spark.eng.Los2 /home/ic
 spark2-submit --master yarn --class com.sccomz.scala.job.spark.eng.Los3 /home/icpap/bin/teos1.jar 8463233 5113566
 spark2-submit --master yarn --class com.sccomz.scala.job.spark.eng.Los2 /home/icpap/bin/teos.jar 8463233
 
-
 #############################################
 # MakeBin
 #############################################
@@ -33,14 +37,12 @@ spark2-submit --master local[*] --properties-file spark-defaults.conf --driver-m
 
 #bf
 spark2-submit --master yarn --jars /home/icpap/lib/ojdbc7.jar,/home/icpap/lib/hiveJdbc11.jar --class com.sccomz.scala.serialize.MakeBfBinFile /home/icpap/bin/teos1.jar 8460965
-spark2-submit --master local[*]  --driver-memory 8g --executor-memory 48g --jars /home/icpap/lib/ojdbc7.jar,/home/icpap/lib/hiveJdbc11.jar --class com.sccomz.scala.serialize.MakeBfBinFile /home/icpap/bin/teos1.jar 8460965
-  
+spark2-submit --master local[*] --driver-memory 8g --executor-memory 48g --jars /home/icpap/lib/ojdbc7.jar,/home/icpap/lib/hiveJdbc11.jar --class com.sccomz.scala.serialize.MakeBfBinFile /home/icpap/bin/teos1.jar 8460965
 
 #############################################
 # BatchJob
 #############################################
 spark2-submit --jars /home/icpap/lib/hiveJdbc11.jar,/home/icpap/lib/ojdbc7.jar --class com.sccomz.scala.schedule.batch.BatchJob /home/icpap/bin/teos.jar 20191111
-
 
 #############################################
 # 기타
